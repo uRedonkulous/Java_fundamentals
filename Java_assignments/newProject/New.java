eef654d4412a5f357ae38ea5c7bacf4fe98e7898
uRedonkulous <kaitoyomma@yahoo.com>
2022-09-27 19:58:35 -0600

    Java Prac

---
+++ Java_assignments/newProject/New.java
@@ -0,0 +1,7 @@
+public class New {
+    public static void main(String[] args) {
+        System.out.println("My name is Coding Dojo");
+        System.out.println("I am 100 Years Old");
+        System.out.println("My hometown is Burbank, CA");
+    }
+}
